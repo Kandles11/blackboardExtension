05a8392334bfcaa73fcee5572f04cfea6b5b6623
Mason Thomas <Kandleseatwicks@gmail.com>
2022-10-25 11:38:54 -0500

    -changed targetNode so banners update correctly -updated manifest to support popup -created popup.html and popup.css

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,24 @@
-console.log("code is working!")
+console.log("code is loaded!")
 
 window.onload = () => {
-    const targetNode = document.getElementById('course-columns-current');
+        const targetNode = document.body;
 
-    const config = { attributes: true, childLIst: true, subtree: true};
-    
-    const callback = (observer) => {
-        const banners = document.getElementsByClassName("course-banner");
-    
-        console.log("code is working!")
-        console.log(banners)
+        const config = { attributes: true, childLIst: true, subtree: true};
         
+        const callback = (observer) => {
+            const banners = document.getElementsByClassName("course-banner");
         
+            console.log("inside the callback!")
+            console.log(banners)
+            
+            for (let i = 0; i < banners.length; i++) {
+                console.log(banners[i])
+                banners[i].style.backgroundImage = "url('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=640:*')";
+            }
+        };
         
-        for (let i = 0; i < banners.length; i++) {
-            console.log(banners[i])
-            banners[i].style.backgroundImage = "url('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=640:*')";
-        }
-    };
-    
-    const observer = new MutationObserver(callback);
-    
-    observer.observe(targetNode, config);
+        const observer = new MutationObserver(callback);
+        
+        observer.observe(targetNode, config);
 }
+    
